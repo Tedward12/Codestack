1c7805090c73f839fe08b78fa3e243fb318c2c83
Code <code@COCTL-41299.local>
2019-11-14 10:37:37 -0800

    got game working

--- Project 3/js/script.js
+++ Project 3/js/script.js
@@ -2,92 +2,6 @@
 
 let injectValue = document.getElementById('inject');
 
-// let totalScore = 0;
-// let incorrect = 0;
-// let totalQuestions = 20;
-// let tQuestions = [];
-// let diff = '../data/data.json';
-// let qNum = 0;
-// let timer = 5;
-// let interval;
-
-
-// let correct = document.getElementById('correct');
-// let counter = document.getElementById('counter');
-// let questions = document.getElementById('questions');
-// let a1 = document.getElementById('a1');
-// let a2 = document.getElementById('a2');
-// let a3 = document.getElementById('a3');
-// let a4 = document.getElementById('a4');
-
-// let buttons = document.getElementsByClassName('btn');
-// for (let i = 0; i < buttons.length; i++) {
-//     buttons[i].addEventListener('click', function (e) {
-//         checkAnswer(e.toElement.innerText);
-//     })
-// }
-
-// function loadJSon(url) {
-//     let xmlhttp = new XMLHttpRequest();
-
-//     xmlhttp.onreadystatechange = function () {
-//         if (this.readyState == 4 && this.status == 200) {
-//             tQuestions = JSON.parse(this.responseText).easy;
-//             console.log(tQuestions);
-//             interval = setInterval(updateTime, 1000)
-//             loadQuestion();
-//         }
-//     };
-//     xmlhttp.open("GET", url, true);
-//     xmlhttp.send();
-// }
-
-// function loadQuestion() {
-//     questions.innerText = tQuestions[qNum].Q;
-//     a1.innerText = tQuestions[qNum].A1;
-//     a2.innerText = tQuestions[qNum].A2;
-//     a3.innerText = tQuestions[qNum].A3;
-//     a4.innerText = tQuestions[qNum].A4;
-// }
-
-// function checkAnswer(answer) {
-//     if (answer === tQuestions[qNum].C) {
-//         totalScore++;
-//     }
-//     else {
-//         incorrect++
-//     }
-//     correct.innerText = `${totalScore}/${totalQuestions}`;
-//     timer = 5;
-//     counter.innerText = timer;
-//     nextQuestion();
-// }
-
-// function nextQuestion() {
-//     if (qNum < totalQuestions) {
-//         qNum++;
-//         loadQuestion();
-//     }
-//     else {
-//         clearInterval(interval);
-//         alert("I have spoken");
-//     }
-// }
-
-// function updateTime() {
-//     timer--;
-//     if (timer == 0) {
-//         timer = 5;
-//         counter.innerText = timer;
-//         nextQuestion();
-//     }
-//     else {
-//         counter.innerText = timer;
-//     }
-// }
-
-// loadJSon(diff);
-
 //----------------------------------------------------------------------------------------------------------------------------------------------//
 
 //----------injecting HTML Pages-----------//
@@ -111,7 +25,7 @@ function loadHTML(url) {
             else if (url == '../inject/title.html') {
                 loadTitlePage(myArr);
             }
-            else if (url == '../inject/endGame.html'){
+            else if (url == '../inject/endGame.html') {
                 loadEndGamePage(myArr);
             }
         }
@@ -120,7 +34,7 @@ function loadHTML(url) {
     xmlhttp.send();
 
 }
-function loadTitlePage(info){
+function loadTitlePage(info) {
     injectValue.innerHTML = info;
 
     let menuBtn = document.getElementById('menu');
@@ -135,7 +49,7 @@ function loadTitlePage(info){
     });
 }
 
-function loadMenuPage(info){
+function loadMenuPage(info) {
     injectValue.innerHTML = info;
 
     let easyModeBtn = document.getElementById('easyMode');
@@ -155,36 +69,107 @@ function loadMenuPage(info){
     });
 }
 
-function loadOptionsPage(info){
+function loadOptionsPage(info) {
     injectValue.innerHTML = info;
 
 }
 
-function loadGamePage(info){
+function loadGamePage(info) {
     injectValue.innerHTML = info;
 
-    let timer = document.getElementById('timer');
-    let question = document.getElementById('question');
-    let a1Btn = document.getElementById('a1');
-    let a2Btn = document.getElementById('a2');
-    let a3Btn = document.getElementById('a3');
-    let a4Btn = document.getElementById('a4');
-    let correctAnswers = document.getElementById('correctAnswers');
+    let totalScore = 0;
+    let incorrect = 0;
+    let totalQuestions = 20;
+    let tQuestions = [];
+    let diff = '../data/data.json';
+    let qNum = 0;
+    let timer = 15;
+    let interval;
 
-    a1Btn.addEventListener('click', function (e) {
-    });
 
-    a2Btn.addEventListener('click', function (e) {
-    });
+    let correct = document.getElementById('correct');
+    let counter = document.getElementById('timer');
+    let questions = document.getElementById('question');
+    let a1 = document.getElementById('a1');
+    let a2 = document.getElementById('a2');
+    let a3 = document.getElementById('a3');
+    let a4 = document.getElementById('a4');
 
-    a3Btn.addEventListener('click', function (e) {
-    });
+    a1.addEventListener('click', function (e) {
+        checkAnswer(e.toElement.innerText);
+    })
+    a2.addEventListener('click', function (e) {
+        checkAnswer(e.toElement.innerText);
+    })
+    a3.addEventListener('click', function (e) {
+        checkAnswer(e.toElement.innerText);
+    })
+    a4.addEventListener('click', function (e) {
+        checkAnswer(e.toElement.innerText);
+    })
 
-    a4Btn.addEventListener('click', function (e) {
-    });
+    function loadJSon(url) {
+        let xmlhttp = new XMLHttpRequest();
+
+        xmlhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                tQuestions = JSON.parse(this.responseText).questions;
+                console.log(tQuestions);
+                interval = setInterval(updateTime, 1000)
+                loadQuestion();
+            }
+        };
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+    }
+
+    function loadQuestion() {
+        questions.innerText = tQuestions[qNum].q;
+        a1.innerText = tQuestions[qNum].a1;
+        a2.innerText = tQuestions[qNum].a2;
+        a3.innerText = tQuestions[qNum].a3;
+        a4.innerText = tQuestions[qNum].a4;
+    }
+
+    function checkAnswer(answer) {
+        if (answer === tQuestions[qNum].correct) {
+            totalScore++;
+        }
+        else {
+            incorrect++
+        }
+        correct.innerText = `${totalScore}/${totalQuestions}`;
+        timer = 15;
+        counter.innerText = timer;
+        nextQuestion();
+    }
+
+    function nextQuestion() {
+        if (qNum < totalQuestions) {
+            qNum++;
+            loadQuestion();
+        }
+        else {
+            clearInterval(interval);
+            alert("I have spoken");
+        }
+    }
+
+    function updateTime() {
+        timer--;
+        if (timer == 0) {
+            timer = 15;
+            counter.innerText = timer;
+            nextQuestion();
+        }
+        else {
+            counter.innerText = timer;
+        }
+    }
+    loadJSon(diff);
 }
 
-function loadEndGamePage(info){
+function loadEndGamePage(info) {
     injectValue.innerHTML = info;
 
 }
