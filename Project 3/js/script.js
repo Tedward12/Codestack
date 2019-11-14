855a269fd75484c68438c91b7abf15a2733ce949
Code <code@COCTL-41299.local>
2019-11-14 10:07:34 -0800

    finished questions

--- Project 3/js/script.js
+++ Project 3/js/script.js
@@ -1,119 +1,95 @@
 
 
+let injectValue = document.getElementById('inject');
+
+// let totalScore = 0;
+// let incorrect = 0;
+// let totalQuestions = 20;
+// let tQuestions = [];
+// let diff = '../data/data.json';
+// let qNum = 0;
+// let timer = 5;
+// let interval;
 
 
-let totalScore = 0;
-let incorrect = 0;
-let totalQuestions = 20;
-let tQuestions = [];
-let diff='../data/data.json';
-let qNum = 0;
-let timer = 5;
-let interval;
+// let correct = document.getElementById('correct');
+// let counter = document.getElementById('counter');
+// let questions = document.getElementById('questions');
+// let a1 = document.getElementById('a1');
+// let a2 = document.getElementById('a2');
+// let a3 = document.getElementById('a3');
+// let a4 = document.getElementById('a4');
 
+// let buttons = document.getElementsByClassName('btn');
+// for (let i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener('click', function (e) {
+//         checkAnswer(e.toElement.innerText);
+//     })
+// }
 
-let correct = document.getElementById('correct');
-let counter = document.getElementById('counter');
-let questions = document.getElementById('questions');
-let a1 = document.getElementById('a1');
-let a2 = document.getElementById('a2');
-let a3 = document.getElementById('a3');
-let a4 = document.getElementById('a4');
+// function loadJSon(url) {
+//     let xmlhttp = new XMLHttpRequest();
 
-let buttons = document.getElementsByClassName('btn');
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function (e) {
-        checkAnswer(e.toElement.innerText);
-    })
-}
+//     xmlhttp.onreadystatechange = function () {
+//         if (this.readyState == 4 && this.status == 200) {
+//             tQuestions = JSON.parse(this.responseText).easy;
+//             console.log(tQuestions);
+//             interval = setInterval(updateTime, 1000)
+//             loadQuestion();
+//         }
+//     };
+//     xmlhttp.open("GET", url, true);
+//     xmlhttp.send();
+// }
 
-function loadJSon(url) {
-    let xmlhttp = new XMLHttpRequest();
-    
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            tQuestions = JSON.parse(this.responseText).easy;
-            console.log(tQuestions);
-            interval = setInterval(updateTime, 1000)
-            loadQuestion();
-        }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-}
+// function loadQuestion() {
+//     questions.innerText = tQuestions[qNum].Q;
+//     a1.innerText = tQuestions[qNum].A1;
+//     a2.innerText = tQuestions[qNum].A2;
+//     a3.innerText = tQuestions[qNum].A3;
+//     a4.innerText = tQuestions[qNum].A4;
+// }
 
-function loadQuestion(){
-    questions.innerText=tQuestions[qNum].Q;
-    a1.innerText = tQuestions[qNum].A1;
-    a2.innerText = tQuestions[qNum].A2;
-    a3.innerText = tQuestions[qNum].A3;
-    a4.innerText = tQuestions[qNum].A4;
-}
+// function checkAnswer(answer) {
+//     if (answer === tQuestions[qNum].C) {
+//         totalScore++;
+//     }
+//     else {
+//         incorrect++
+//     }
+//     correct.innerText = `${totalScore}/${totalQuestions}`;
+//     timer = 5;
+//     counter.innerText = timer;
+//     nextQuestion();
+// }
 
-function checkAnswer(answer){
-    if(answer === tQuestions[qNum].C){
-        totalScore++;
-    }
-    else{
-        incorrect++
-    }
-    correct.innerText=`${totalScore}/${totalQuestions}`;
-    timer=5;
-    counter.innerText=timer;
-    nextQuestion();
-}
+// function nextQuestion() {
+//     if (qNum < totalQuestions) {
+//         qNum++;
+//         loadQuestion();
+//     }
+//     else {
+//         clearInterval(interval);
+//         alert("I have spoken");
+//     }
+// }
 
-function nextQuestion(){
-    if(qNum<totalQuestions){
-        qNum++;
-        loadQuestion();
-    }
-    else{
-        clearInterval(interval);
-        alert("I have spoken");
-    }
-}
+// function updateTime() {
+//     timer--;
+//     if (timer == 0) {
+//         timer = 5;
+//         counter.innerText = timer;
+//         nextQuestion();
+//     }
+//     else {
+//         counter.innerText = timer;
+//     }
+// }
 
-function updateTime(){
-    timer--;
-    if(timer==0){
-        timer = 5;
-        counter.innerText = timer;
-        nextQuestion();
-    }
-    else{
-        counter.innerText=timer;
-    }
-}
-
-loadJSon(diff);
+// loadJSon(diff);
 
 //----------------------------------------------------------------------------------------------------------------------------------------------//
-//---------Set and Get Variables----------//
 
-let injectValue = document.getElementById('inject');
-let menuBtn = document.getElementById('menu');
-let optionsBtn = document.getElementById('options');
-let titleBtn = document.getElementById('title');
-let easyModeBtn = document.getElementById('easyMode');
-let hardModeBtn = document.getElementById('hardMode');
-
-//----------Add Event Listeners---------//
-domBtn.addEventListener('click', function (e) {
-    loadHTML('../inject/menu.html');
-});
-
-randomBtn.addEventListener('click', function (e) {
-    loadHTML('../inject/options.html');
-});
-
-directoryBtn.addEventListener('click', function (e) {
-    loadHTML('../inject/easyMode.html');
-});
-
-directoryBtn.addEventListener('click', function (e) {
-    loadHTML('../inject/hardMode.html');
-});
 //----------injecting HTML Pages-----------//
 
 function loadHTML(url) {
@@ -121,20 +97,22 @@ function loadHTML(url) {
 
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            //When Injecting HTML, you don't need a JSON.parse
             let myArr = this.responseText;
-            //conditional statement to load the proper page items
+            console.log(url);
             if (url == '../inject/menu.html') {
-                loadDomPage(myArr);
+                loadMenuPage(myArr);
             }
-            if (url == '../inject/options.html') {
-                loadDomPage(myArr);
+            else if (url == '../inject/options.html') {
+                loadOptionsPage(myArr);
             }
-            if (url == '../inject/easyMode.html') {
-                loadDomPage(myArr);
+            else if (url == '../inject/game.html') {
+                loadGamePage(myArr);
             }
-            if (url == '../inject/hardMode.html') {
-                loadDomPage(myArr);
+            else if (url == '../inject/title.html') {
+                loadTitlePage(myArr);
+            }
+            else if (url == '../inject/endGame.html'){
+                loadEndGamePage(myArr);
             }
         }
     };
@@ -142,12 +120,73 @@ function loadHTML(url) {
     xmlhttp.send();
 
 }
+function loadTitlePage(info){
+    injectValue.innerHTML = info;
 
-//-----------Loading Pages -----------------//
+    let menuBtn = document.getElementById('menu');
+    let optionsBtn = document.getElementById('options');
 
-//Dom Manipulation Page
-function loadDomPage(info) {
-    //Injecting your html into the injectValue 
+    menuBtn.addEventListener('click', function (e) {
+        loadHTML('../inject/menu.html');
+    });
+
+    optionsBtn.addEventListener('click', function (e) {
+        loadHTML('../inject/options.html');
+    });
+}
+
+function loadMenuPage(info){
+    injectValue.innerHTML = info;
+
+    let easyModeBtn = document.getElementById('easyMode');
+    let hardModeBtn = document.getElementById('hardMode');
+    let backBtn = document.getElementById('back');
+
+    easyModeBtn.addEventListener('click', function (e) {
+        loadHTML('../inject/game.html');
+    });
+
+    hardModeBtn.addEventListener('click', function (e) {
+        loadHTML('../inject/game.html');
+    });
+
+    backBtn.addEventListener('click', function (e) {
+        loadHTML('../inject/title.html');
+    });
+}
+
+function loadOptionsPage(info){
     injectValue.innerHTML = info;
 
 }
+
+function loadGamePage(info){
+    injectValue.innerHTML = info;
+
+    let timer = document.getElementById('timer');
+    let question = document.getElementById('question');
+    let a1Btn = document.getElementById('a1');
+    let a2Btn = document.getElementById('a2');
+    let a3Btn = document.getElementById('a3');
+    let a4Btn = document.getElementById('a4');
+    let correctAnswers = document.getElementById('correctAnswers');
+
+    a1Btn.addEventListener('click', function (e) {
+    });
+
+    a2Btn.addEventListener('click', function (e) {
+    });
+
+    a3Btn.addEventListener('click', function (e) {
+    });
+
+    a4Btn.addEventListener('click', function (e) {
+    });
+}
+
+function loadEndGamePage(info){
+    injectValue.innerHTML = info;
+
+}
+
+loadHTML('../inject/title.html');
