b4cf7cbf8a37fec5c7f4e005a067fddf9a6ba20f
Code <code@COCTL-41299.local>
2019-11-14 14:52:53 -0800

    almost done

--- Project 3/js/script.js
+++ Project 3/js/script.js
@@ -1,8 +1,7 @@
 
 
 let injectValue = document.getElementById('inject');
-
-//----------------------------------------------------------------------------------------------------------------------------------------------//
+let totalScore = 0;
 
 //----------injecting HTML Pages-----------//
 
@@ -20,7 +19,10 @@ function loadHTML(url) {
                 loadOptionsPage(myArr);
             }
             else if (url == '../inject/game.html') {
-                loadGamePage(myArr);
+                loadEasyPage(myArr);
+            }
+            else if (url == '../inject/hard.html') {
+                loadHardPage(myArr);
             }
             else if (url == '../inject/title.html') {
                 loadTitlePage(myArr);
@@ -34,6 +36,9 @@ function loadHTML(url) {
     xmlhttp.send();
 
 }
+
+//------------------------------------- Load Title Page -------------------------------------//
+
 function loadTitlePage(info) {
     injectValue.innerHTML = info;
 
@@ -49,6 +54,8 @@ function loadTitlePage(info) {
     });
 }
 
+//------------------------------------- Load Menu Page -------------------------------------//
+
 function loadMenuPage(info) {
     injectValue.innerHTML = info;
 
@@ -61,7 +68,7 @@ function loadMenuPage(info) {
     });
 
     hardModeBtn.addEventListener('click', function (e) {
-        loadHTML('../inject/game.html');
+        loadHTML('../inject/hard.html');
     });
 
     backBtn.addEventListener('click', function (e) {
@@ -69,21 +76,129 @@ function loadMenuPage(info) {
     });
 }
 
+//------------------------------------- Load Options Page -------------------------------------//
+
 function loadOptionsPage(info) {
     injectValue.innerHTML = info;
 
+    let backBtn = document.getElementById('back');
+
+    backBtn.addEventListener('click', function (e) {
+        loadHTML('../inject/title.html');
+    });
 }
 
-function loadGamePage(info) {
+//------------------------------------- Load Easy Page -------------------------------------//
+
+function loadEasyPage(info) {
     injectValue.innerHTML = info;
 
-    let totalScore = 0;
     let incorrect = 0;
     let totalQuestions = 20;
     let tQuestions = [];
     let diff = '../data/data.json';
     let qNum = 0;
     let timer = 15;
+    let interval;
+
+
+    let correct = document.getElementById('correct');
+    let counter = document.getElementById('timer');
+    let questions = document.getElementById('question');
+    let a1 = document.getElementById('a1');
+    let a2 = document.getElementById('a2');
+    let a3 = document.getElementById('a3');
+    let a4 = document.getElementById('a4');
+
+    a1.addEventListener('click', function (e) {
+        checkAnswer(e.toElement.innerText);
+    })
+    a2.addEventListener('click', function (e) {
+        checkAnswer(e.toElement.innerText);
+    })
+    a3.addEventListener('click', function (e) {
+        checkAnswer(e.toElement.innerText);
+    })
+    a4.addEventListener('click', function (e) {
+        checkAnswer(e.toElement.innerText);
+    })
+
+    function loadJSon(url) {
+        let xmlhttp = new XMLHttpRequest();
+
+        xmlhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                tQuestions = JSON.parse(this.responseText).questions;
+                console.log(tQuestions);
+                interval = setInterval(updateTime, 1000)
+                loadQuestion();
+            }
+        };
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+    }
+
+    function loadQuestion() {
+        questions.className = "size35 col-12 bgWhite d-flex justify-content-center mb-0";
+        counter.className = "size35 col-4 bgWhite d-flex justify-content-center";
+        correct.className = "size35 col-4 bgWhite d-flex justify-content-center mb-0";
+
+        questions.innerText = tQuestions[qNum].q;
+        a1.innerText = tQuestions[qNum].a1;
+        a2.innerText = tQuestions[qNum].a2;
+        a3.innerText = tQuestions[qNum].a3;
+        a4.innerText = tQuestions[qNum].a4;
+    }
+
+    function checkAnswer(answer) {
+        if (answer === tQuestions[qNum].correct) {
+            totalScore++;
+        }
+        else {
+            incorrect++
+        }
+        correct.innerText = `${totalScore}/${totalQuestions}`;
+        timer = 15;
+        counter.innerText = timer;
+        nextQuestion();
+    }
+
+    function nextQuestion() {
+        if (qNum < totalQuestions) {
+            qNum++;
+            loadQuestion();
+        }
+        else {
+            clearInterval(interval);
+            loadHTML('../inject/endGame.html');
+        }
+    }
+
+    function updateTime() {
+        timer--;
+        if (timer == 0) {
+            timer = 15;
+            counter.innerText = timer;
+            nextQuestion();
+        }
+        else {
+            counter.innerText = timer;
+        }
+    }
+    loadJSon(diff);
+}
+
+//------------------------------------- Load Hard Page -------------------------------------//
+
+function loadHardPage(info) {
+    injectValue.innerHTML = info;
+
+    let incorrect = 0;
+    let totalQuestions = 20;
+    let tQuestions = [];
+    let diff = '../data/data.json';
+    let qNum = 0;
+    let timer = 5;
     let interval;
 
 
@@ -139,7 +254,7 @@ function loadGamePage(info) {
             incorrect++
         }
         correct.innerText = `${totalScore}/${totalQuestions}`;
-        timer = 15;
+        timer = 5;
         counter.innerText = timer;
         nextQuestion();
     }
@@ -151,14 +266,14 @@ function loadGamePage(info) {
         }
         else {
             clearInterval(interval);
-            alert("I have spoken");
+            loadEndGamePage();
         }
     }
 
     function updateTime() {
         timer--;
         if (timer == 0) {
-            timer = 15;
+            timer = 5;
             counter.innerText = timer;
             nextQuestion();
         }
@@ -169,9 +284,20 @@ function loadGamePage(info) {
     loadJSon(diff);
 }
 
+//------------------------------------- Load End Game Page -------------------------------------//
+
 function loadEndGamePage(info) {
     injectValue.innerHTML = info;
 
+    let correct = document.getElementById('endGameCorrect')
+    let backBtn = document.getElementById('back');
+
+    backBtn.addEventListener('click', function (e) {
+        loadHTML('../inject/title.html');
+        totalScore = 0;
+    });
+    correct.innerHTML = "col-12 bgWhite d-flex justify-content-center";
+    correct.innerText = `${totalScore}/20`;
 }
 
 loadHTML('../inject/title.html');
