b1f58e965a8de2ed17cb0ef85ac1565e21e573f6
Code <code@COCTL-41299.local>
2019-11-15 11:23:34 -0800

    finish

--- Project 3/js/script.js
+++ Project 3/js/script.js
@@ -4,6 +4,7 @@ let injectValue = document.getElementById('inject');
 let totalScore = 0;
 let totalQuestions = 20;
 let tQuestions = [];
+let bookmarks = 0;
 
 
 //----------injecting HTML Pages-----------//
@@ -93,20 +94,19 @@ function loadOptionsPage(info) {
 
 //------------------------------------- Get Random Questions -------------------------------------//
 
-function randomQuestions(q){
+function randomQuestions(q) {
     //q is triviaQFull
     let qNum = 0;
     let rQuestions = [];
 
-    for(let i=0; i<totalQuestions; i++)
-    {
+    for (let i = 0; i < totalQuestions; i++) {
         //SHUFFLE
-        qNum = Math.floor(Math.random()*q.length);
+        qNum = Math.floor(Math.random() * q.length);
         //console.log(qNum);
         //add from eq! json array to triviaQ
         rQuestions.push(q[qNum]);
         //Remove the item from ezQ
-        q.splice(qNum,1);
+        q.splice(qNum, 1);
     }
     return rQuestions;
 }
@@ -119,6 +119,115 @@ function loadEasyPage(info) {
     let diff = '../data/data.json';
     let qNum = 0;
     let timer = 15;
+    let interval;
+    let triviaQ = [];
+    let count = 0;
+
+    let correct = document.getElementById('correct');
+    let counter = document.getElementById('timer');
+    let questions = document.getElementById('question');
+    let a1 = document.getElementById('a1');
+    let a2 = document.getElementById('a2');
+    let a3 = document.getElementById('a3');
+    let a4 = document.getElementById('a4');
+    let bookmark = document.getElementById('bookmark');
+
+    a1.addEventListener('click', function (e) {
+        checkAnswer(e.toElement.innerText);
+    })
+    a2.addEventListener('click', function (e) {
+        checkAnswer(e.toElement.innerText);
+    })
+    a3.addEventListener('click', function (e) {
+        checkAnswer(e.toElement.innerText);
+    })
+    a4.addEventListener('click', function (e) {
+        checkAnswer(e.toElement.innerText);
+    })
+
+    function loadJSon(url) {
+        let xmlhttp = new XMLHttpRequest();
+
+        xmlhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                tQuestions = JSON.parse(this.responseText).questions;
+                interval = setInterval(updateTime, 1000);
+                triviaQ = randomQuestions(tQuestions);
+                loadQuestion();
+            }
+        };
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+    }
+
+    function loadQuestion() {
+        questions.className = "size35 col-12 bgWhite d-flex justify-content-center mb-0";
+        counter.className = "size35 col-4 bgWhite d-flex justify-content-center";
+        correct.className = "size35 col-4 bgWhite d-flex justify-content-center mb-0";
+
+        questions.innerText = triviaQ[qNum].q;
+        a1.innerText = triviaQ[qNum].a1;
+        a2.innerText = triviaQ[qNum].a2;
+        a3.innerText = triviaQ[qNum].a3;
+        a4.innerText = triviaQ[qNum].a4;
+    }
+
+    function checkAnswer(answer) {
+        if (answer === triviaQ[qNum].correct) {
+            totalScore++;
+            count++;
+            if (count == 3){
+                bookmark.innerText = "You collected a covenant bookmark!"; 
+                count = 0;
+                bookmarks++;
+                setTimeout(() => {
+                    bookmark.innerText = "";
+                }, 5000);
+            }
+        }
+        else{
+            count = 0;
+        }
+        correct.innerText = `${totalScore}/${totalQuestions}`;
+        timer = 15;
+        counter.innerText = timer;
+        nextQuestion();
+    }
+
+    function nextQuestion() {
+        qNum++;
+        console.log(qNum);
+        if (qNum < totalQuestions) {
+            loadQuestion();
+        }
+        else {
+            clearInterval(interval);
+            loadHTML('../inject/endGame.html');
+        }
+    }
+
+    function updateTime() {
+        timer--;
+        if (timer == 0) {
+            timer = 15;
+            counter.innerText = timer;
+            nextQuestion();
+        }
+        else {
+            counter.innerText = timer;
+        }
+    }
+    loadJSon(diff);
+}
+
+//------------------------------------- Load Hard Page -------------------------------------//
+
+function loadHardPage(info) {
+    injectValue.innerHTML = info;
+
+    let diff = '../data/data.json';
+    let qNum = 0;
+    let timer = 5;
     let interval;
     let triviaQ = [];
 
@@ -176,7 +285,7 @@ function loadEasyPage(info) {
             totalScore++;
         }
         correct.innerText = `${totalScore}/${totalQuestions}`;
-        timer = 15;
+        timer = 5;
         counter.innerText = timer;
         nextQuestion();
     }
@@ -190,99 +299,6 @@ function loadEasyPage(info) {
         else {
             clearInterval(interval);
             loadHTML('../inject/endGame.html');
-        }
-    }
-
-    function updateTime() {
-        timer--;
-        if (timer == 0) {
-            timer = 15;
-            counter.innerText = timer;
-            nextQuestion();
-        }
-        else {
-            counter.innerText = timer;
-        }
-    }
-    loadJSon(diff);
-}
-
-//------------------------------------- Load Hard Page -------------------------------------//
-
-function loadHardPage(info) {
-    injectValue.innerHTML = info;
-
-    let diff = '../data/data.json';
-    let qNum = 0;
-    let timer = 5;
-    let interval;
-
-
-    let correct = document.getElementById('correct');
-    let counter = document.getElementById('timer');
-    let questions = document.getElementById('question');
-    let a1 = document.getElementById('a1');
-    let a2 = document.getElementById('a2');
-    let a3 = document.getElementById('a3');
-    let a4 = document.getElementById('a4');
-
-    a1.addEventListener('click', function (e) {
-        checkAnswer(e.toElement.innerText);
-    })
-    a2.addEventListener('click', function (e) {
-        checkAnswer(e.toElement.innerText);
-    })
-    a3.addEventListener('click', function (e) {
-        checkAnswer(e.toElement.innerText);
-    })
-    a4.addEventListener('click', function (e) {
-        checkAnswer(e.toElement.innerText);
-    })
-
-    function loadJSon(url) {
-        let xmlhttp = new XMLHttpRequest();
-
-        xmlhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                tQuestions = JSON.parse(this.responseText).questions;
-                console.log(tQuestions);
-                interval = setInterval(updateTime, 1000)
-                loadQuestion();
-            }
-        };
-        xmlhttp.open("GET", url, true);
-        xmlhttp.send();
-    }
-
-    function loadQuestion() {
-        questions.innerText = tQuestions[qNum].q;
-        a1.innerText = tQuestions[qNum].a1;
-        a2.innerText = tQuestions[qNum].a2;
-        a3.innerText = tQuestions[qNum].a3;
-        a4.innerText = tQuestions[qNum].a4;
-    }
-
-    function checkAnswer(answer) {
-        if (answer === tQuestions[qNum].correct) {
-            totalScore++;
-        }
-        else {
-            incorrect++
-        }
-        correct.innerText = `${totalScore}/${totalQuestions}`;
-        timer = 5;
-        counter.innerText = timer;
-        nextQuestion();
-    }
-
-    function nextQuestion() {
-        if (qNum < totalQuestions) {
-            qNum++;
-            loadQuestion();
-        }
-        else {
-            clearInterval(interval);
-            loadEndGamePage();
         }
     }
 
@@ -307,10 +323,17 @@ function loadEndGamePage(info) {
 
     let correct = document.getElementById('endGameCorrect')
     let backBtn = document.getElementById('back');
+    let bookmarkCount = document.getElementById('bookmarkCount');
+    let points = document.getElementById('points');
+
+    bookmarkCount.innerText = bookmarks;
+
+    points.innerText = totalScore + (bookmarks*3);
 
     backBtn.addEventListener('click', function (e) {
         loadHTML('../inject/title.html');
         totalScore = 0;
+        bookmarks = 0;
     });
     correct.innerHTML = "col-12 bgWhite d-flex justify-content-center";
     correct.innerText = `${totalScore}/20`;
