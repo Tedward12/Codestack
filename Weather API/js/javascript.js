58c3b5f6d8396833804892b185ad6ca6a2639bdf
Code <code@COCTL-41299.local>
2019-12-10 14:17:49 -0800

    gets weather info

--- Weather API/js/javascript.js
+++ Weather API/js/javascript.js
@@ -5,15 +5,9 @@
 
 
 
-
-
-
-
-
-
-
+//--------------------------------------- Inject Pages -----------------------------------------//
 let injectValue = document.getElementById('inject');
-let body = document.getElementById('body')
+let body = document.getElementById('body');
 
 
 function loadHTML(url) {
@@ -36,6 +30,20 @@ function loadHTML(url) {
 }
 function loadMainPage(info) {
     injectValue.innerHTML = info;
+    let searchBtn = document.getElementById('searchBtn');
+
+    ///------load your variables for all to use-------//
+    //-------------Add Event Listeners------------//
+    searchBtn.addEventListener('click', function (e) {
+        let searchedItem = document.getElementById('searchTxt');
+        let url_pt1 = "https://api.openweathermap.org/data/2.5/weather?q="
+        let url_city_pt2 = searchedItem.value;
+        let url_temp_pt3 = "&units=imperial";
+        let url_key_pt4 = "&appid=99649a8b829f6ea98a24e0ef1fd6b301";
+        let fullURL = url_pt1 + url_city_pt2 + url_temp_pt3 + url_key_pt4;
+        loadJSON(fullURL);
+        // searchedItem.value = null;
+    });
 
     let cityBtn = document.getElementById('city');
     body.className = "";
@@ -54,5 +62,28 @@ function loadWeatherPage(info) {
     });
 }
 
-
 loadHTML('../inject/main.html');
+
+
+
+
+//---------Load Your JSON Weather File--------//
+function loadJSON(URL) {
+    let xmlhttp = new XMLHttpRequest();
+    //Put your weather API URL and KEY here
+    //let url = "";
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let myArr = JSON.parse(this.responseText);
+            getWeather(myArr);
+        }
+        else if (this.readyState == 4 && this.status != 200) {
+            alert('Input not available.')
+        }
+    };
+    xmlhttp.open("GET", URL, true);
+    xmlhttp.send();
+}
+function getWeather(info) {
+    console.log(info);
+}
