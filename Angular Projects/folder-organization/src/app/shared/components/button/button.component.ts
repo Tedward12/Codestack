929f1c6768f5e398e587125ab89ebeb911cc77a5
Code <code@COCTL-41299.local>
2020-01-15 14:33:15 -0800

    create a add to cart process

---
+++ Angular Projects/folder-organization/src/app/shared/components/button/button.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+
+export class ButtonComponent implements OnInit {
+  @Input() btnClass: string;
+
+  @Input() btnLabel: string;
+  @Output() clickButton = new EventEmitter<any>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick(){
+    this.clickButton.emit();
+  }
+}
---
+++ Angular Projects/folder-organization/src/app/shared/services/cart.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  private cartCount = 0;
+  public $cartCount = new BehaviorSubject<number>(this.cartCount);
+
+  constructor() { }
+
+  addToCart(){
+    this.cartCount++;
+    this.$cartCount.next(this.cartCount);
+  }
+}
