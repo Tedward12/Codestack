019de34cb6c2f91ff8f055bf6404e574184f8d10
Code <code@COCTL-41299.local>
2020-01-21 11:47:16 -0800

    bart

--- Angular Projects/bart/src/app/app.module.ts
+++ Angular Projects/bart/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './views/pages/header/header.component';
+import { MainComponent } from './views/pages/main/main.component';
+import { FooterComponent } from './views/pages/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Angular Projects/bart/src/app/views/pages/main/main.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  apiURL = 'http://api.bart.gov/api/etd.aspx?cmd=etd&orig=RICH&key=Z2R9-58Q2-9MUT-DWE9&json=y';
+
+  constructor(private dataS: DataService) { }
+
+  ngOnInit() {
+    this.dataS.getURL(this.apiURL).subscribe(x => {console.log(x)})
+  }
+
+}
