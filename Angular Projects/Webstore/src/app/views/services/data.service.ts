b22539da441bfd17810a88de213bd1c3b7a27f1c
Code <code@COCTL-41299.local>
2020-01-27 10:30:43 -0800

    fix scroll bar

--- Angular Projects/Webstore/src/app/views/services/data.service.ts
+++ Angular Projects/Webstore/src/app/views/services/data.service.ts
@@ -5,5 +5,35 @@ import { Injectable } from '@angular/core';
 })
 export class DataService {
 
+  items: [
+    {pName:"Pokeball",price:"$200",img:"assets/images/pokeBall.png"},
+    {pName:"Great Ball",price:"$600",img:"assets/images/greatBall.png"},
+    {pName:"Ultra Ball",price:"$1,200",img:"assets/images/ultraBall.png"},
+    {pName:"Premier Ball",price:"$250",img:"assets/images/premierBall.png"},
+    {pName:"Luxury Ball",price:"$3,000",img:"assets/images/luxuryBall.png"},
+    {pName:"Master Ball",price:"$250,000",img:"assets/images/masterBall.png"},
+    {pName:"Quick Ball",price:"$1,000",img:"assets/images/quickBall.png"},
+    {pName:"Repeat Ball",price:"$1,000",img:"assets/images/repeatBall.png"},
+    {pName:"Timer Ball",price:"$1,000",img:"assets/images/timerBall.png"},
+    {pName:"Dusk Ball",price:"$1,000",img:"assets/images/duskBall.png"},
+    {pName:"Safari Ball",price:"$1,000",img:"assets/images/safariBall.png"},
+    {pName:"NestBall",price:"$1,000",img:"assets/images/nestBall.png"},
+    {pName:"Heal Ball",price:"$1,000",img:"assets/images/healBall.png"},
+    {pName:"Net Ball",price:"$1,000",img:"assets/images/netBall.png"},
+    {pName:"Dive Ball",price:"$1,000",img:"assets/images/diveBall.png"},
+    {pName:"Love Ball",price:"$1,000",img:"assets/images/lopveBall.png"},
+    {pName:"Friend Ball",price:"$1,000",img:"assets/images/friendBall.png"},
+    {pName:"Sport Ball",price:"$1,000",img:"assets/images/sportBall.png"},
+    {pName:"Lure Ball",price:"$1,000",img:"assets/images/lureBall.png"},
+    {pName:"Heavy Ball",price:"$1,000",img:"assets/images/heavyBall.png"},
+    {pName:"Moon Ball",price:"$1,000",img:"assets/images/moonBall.png"},
+    {pName:"Park Ball",price:"$1,000",img:"assets/images/parkBall.png"},
+    {pName:"Fast Ball",price:"$1,000",img:"assets/images/fastBall.png"},
+    {pName:"Level Ball",price:"$1,000",img:"assets/images/levelBall.png"},
+    {pName:"Dream Ball",price:"$25,000",img:"assets/images/dreamBall.png"},
+    {pName:"Cherish Ball",price:"$25,000",img:"assets/images/cherishBall.png"},
+    {pName:"Beast Ball",price:"$100,000",img:"assets/images/beastBall.png"},
+
+  ]
   constructor() { }
 }
