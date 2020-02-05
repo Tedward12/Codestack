8d00c03ae8544e3bed9e56105e3ffd8fa4ac23b5
Code <code@COCTL-41299.local>
2020-02-05 11:15:04 -0800

    stuff

--- Angular Projects/Portfolio/src/app/app-routing.module.ts
+++ Angular Projects/Portfolio/src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { AboutMeComponent } from './pages/about-me/about-me.component';
-import { ProjectsComponent } from './pages/projects/projects.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
   {path: 'pages/home', component: HomeComponent},
   {path: 'pages/about-me', component: AboutMeComponent},
-  {path: 'pages/projects', component: ProjectsComponent},
   {path: '', redirectTo: 'pages/home', pathMatch: 'full'},
   { path: '**', component:  PageNotFoundComponent}
 ];
