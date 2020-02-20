af84fb43ec9103e632d533c29db761699c329609
Code <code@COCTL-41299.local>
2020-02-20 10:25:01 -0800

    gen new ion

--- Ionic/Ionic Practice/myApp/src/app/tab1/tab1.page.ts
+++ Ionic/Ionic Practice/myApp/src/app/tab1/tab1.page.ts
@@ -1,10 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { AlertController } from '@ionic/angular';
-
-
-
-
-
+import { IonReorderGroup } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -12,6 +8,8 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  @ViewChild(IonReorderGroup,{static: false}) reorderGroup: IonReorderGroup;
+
 
   constructor(public alertController: AlertController) { }
 
@@ -47,4 +45,18 @@ export class Tab1Page {
     await alert.present();
   }
 
+  doReorder(ev: any) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    ev.detail.complete();
+  }
+
+  toggleReorderGroup() {
+    this.reorderGroup.disabled = !this.reorderGroup.disabled;
+  }
 }
